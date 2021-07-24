eea2888acb017eb5b894992d1c2a5dccaaa2d237
austinlewis <austinlewis1996@gmail.com>
2021-07-25 01:58:04 +0530

    includes new 'Arrays' package - solved 'contains duplicate'.

---
+++ src/trial/code/leet/arrays/ContainsDuplicate.java
@@ -0,0 +1,41 @@
+package trial.code.leet.arrays;
+
+// https://leetcode.com/problems/contains-duplicate/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] value = new int[]{-1, -2, -3, 1, 2, 3, -1, 4};
+        System.out.println(containsDuplicate(value));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        /* using Arrays.stream
+        Leetcode Submission - https://leetcode.com/submissions/detail/527707931/
+        Runtime: 7 ms
+        Memory Usage: 46.4 MB
+
+        return Arrays.stream(nums).distinct().count() != nums.length;
+        */
+
+        /* using forEach
+        Leetcode Submission - https://leetcode.com/submissions/detail/527712796/
+        Runtime: 4 ms
+        Memory Usage: 42.8 MB
+
+
+        Set<Integer> uniqueValues = new HashSet<Integer>();
+        for (int num : nums) {
+            if (!uniqueValues.add(num)) {
+                return true;
+            }
+        }
+        */
+
+        //TODO: Why does the forEach method take less runtime compared to in-built Arrays.stream(); Is it because of the distinct(), if so how much and why?
+
+        return false;
+    }
+}
